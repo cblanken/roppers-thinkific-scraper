a5df1d6aacae97ffd1771bc2fc880a16299fdd88
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-02-14 10:23:49 -0500

    Add convenience scripts (download_all.sh and convert_all.sh)

---
+++ convert_all.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python convert.py ./courses/ComputingFundamentals/ ./markdown/ComputingFundamentals/
+python convert.py ./courses/SecurityFundamentals/ ./markdown/SecurityFundamentals/
+python convert.py ./courses/PracticalNetworking/ ./markdown/PracticalNetworking/
+python convert.py ./courses/CaptureTheFlagBootCamp/ ./markdown/CaptureTheFlagBootCamp/
---
+++ download_all.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python scrape.py 'https://www.roppers.org' ./courses/ComputingFundamentals/
+python scrape.py 'https://www.roppers.org' ./courses/SecurityFundamentals/
+python scrape.py 'https://www.roppers.org' ./courses/PracticalNetworking/
+python scrape.py 'https://www.roppers.org' ./courses/CaptureTheFlagBootCamp/
