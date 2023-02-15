b59ff004a781fa4da0c57551f3e705162107767e
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-02-15 09:30:08 -0500

    Reinsert iframe contents into markdown after conversion

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import sys
-import pandoc
 from pathlib import Path
-import mdformat
+import pandoc
+from bs4 import BeautifulSoup 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -19,16 +19,29 @@ if __name__ == "__main__":
                     out_file_path = Path(out_dir, chapter_dir.name, f"{lesson.stem}.md")
                     print(f"  - {out_file_path}")
 
-                    with lesson.open("r", encoding="utf-8") as chapter_contents:
+                    with lesson.open("r", encoding="utf-8") as lesson_file:
+                        source = lesson_file.read()
                         html = pandoc.read(
-                            file=chapter_contents,
-                            format="html"
+                            source=source,
+                            format="html+raw_html"
                         )
-                    
+
                         # Convert to GitHub-flavored markdown (gfm)
                         markdown_content = pandoc.write(
                             doc=html,
                             file=out_file_path,
                             format="gfm-raw_html",
-                            # format="markdown_strict-raw_html",
                         )
+
+                        # Capture any iframes in source file
+                        soup = BeautifulSoup(source, "html.parser")
+                        iframes = "\n".join([str(ele) for ele in soup.select("iframe")])
+
+                        # Re-embed iframes
+                        with open(out_file_path, "r+") as markdown_file:
+                            data = markdown_file.read()
+                            markdown_file.seek(0)
+                            markdown_file.write(iframes + '\n')
+                            markdown_file.write(data)
+
+                    
